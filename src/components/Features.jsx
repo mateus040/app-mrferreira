51c385e3a79b27c2b212231bd521a1fd8c0e84b5
mateus040 <mateusgabrielmoreno264@gmail.com>
2023-12-15 14:03:36 -0300

    feat: criando componente features

---
+++ src/components/Features.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "../styles/features.css";
+import cadeira1 from "../assets/cadeira1.png";
+import cadeira2 from "../assets/cadeira2.png";
+import cadeira3 from "../assets/cadeira3.png";
+
+const Features = () => {
+    return (
+        <div className="features" id="features">
+            <h1 className="heading"> our <span>features</span> </h1>
+
+            <div className="box-container">
+                <div className="box">
+                    <img src={cadeira1}></img>
+                    <h3>Fresh and organic</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt, earum!</p>
+                    <a href="" className="btn">read more</a>
+                </div>
+
+                <div className="box">
+                    <img src={cadeira2}></img>
+                    <h3>Fresh and organic</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt, earum!</p>
+                    <a href="" className="btn">read more</a>
+                </div>
+
+                <div className="box">
+                    <img src={cadeira3}></img>
+                    <h3>Fresh and organic</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt, earum!</p>
+                    <a href="" className="btn">read more</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Features;
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import Header from "../components/Header";
 import Home from "../components/Home";
+import Features from "../components/Features";
 
 const Main = () => {
     return (
 
         <div className="app">
-            <header className="header-content">
+            <header className="header">
                 <Header />
             </header>
 
             <section className="home">
                 <Home />
+            </section>
+
+            <section className="features">
+                <Features />
             </section>
         </div>
 

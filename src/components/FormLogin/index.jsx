bc4f95c02f0cbf71942c0dab75ad6d936bf7ac24
mateus040 <mateusgabrielmoreno264@gmail.com>
2024-01-14 12:31:06 -0300

    feat: testes de autenticação

--- src/components/FormLogin/index.jsx
+++ src/components/FormLogin/index.jsx
@@ -1,12 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
+import { useNavigate } from "react-router-dom";
+import { toast } from 'react-toastify';
+import axios from "axios";
 
 const FormLogin = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const navigate = useNavigate();
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post("http://127.0.0.1:800/api/login", {
+                email, password,
+            });
+
+            const token = response.data.token;
+
+            localStorage.setItem("token", token);
+
+            navigate("/home/admin");
+        } catch (err) {
+            toast.error("Erro ao fazer login: " + err.response.data.message, {
+                theme: "colored",
+                style: {
+                    fontSize: '1.6rem',
+                },
+            });
+            console.error("Erro ao fazer login: ", err.response.data.message);
+        }
+    }
+
     return (
-        <form action="" className="login-form">
+        <form onSubmit={handleLogin} className="login-form">
             <h3>Login Now</h3>
-            <input type="email" placeholder="Your Email" className="box"></input>
-            <input type="password" placeholder="Your Password" className="box"></input>
+            <input 
+                type="email" 
+                placeholder="Your Email"
+                className="box"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <input 
+                type="password" 
+                placeholder="Your Password" 
+                className="box"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
             <p>Forget your password <a href="">click here</a></p>
             <input type="submit" placeholder="Entrar" className="btn"></input>
         </form>
